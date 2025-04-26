40c6a61de30448179ae74301b545f2dc651b5aaa
Mikenifty <stuartneff@yahoo.com>
2025-04-26 19:59:12 +0800

    Linting 11

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -136,7 +136,7 @@ def add_review(request):
     if (request.user.is_anonymous) is False:
         data = json.loads(request.body)
         try:
-            # response = 
+            # response =
             post_review(data)
             return JsonResponse({"status": 200})
         except Exception:
